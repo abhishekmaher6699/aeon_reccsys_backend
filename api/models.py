650fbde328d0d4a47c8ec8101e4256505debb65e
Abhishek Maher <abhishekmaher2004@gmail.com>
2024-12-25 14:18:02 +0530

    added images in the output

--- api/models.py
+++ api/models.py
@@ -10,7 +10,7 @@ def recommend_articles(url, sim_scores, df, url_to_id, top_n=10):
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
     sim_scores = sim_scores[1:top_n+1]
     top_indices = [i[0] for i in sim_scores]
-    recommended_articles = df.iloc[top_indices][['url', 'title', 'content']]
+    recommended_articles = df.iloc[top_indices][['url', 'title', 'content', 'image']]
     recommended_articles['content'] = recommended_articles['content'].apply(lambda x: x[:100] if isinstance(x, str) else x)
     return recommended_articles.T.to_dict()
 
@@ -21,6 +21,6 @@ def recommend_articles_using_prompt(prompt, vectorizer, matrix, df, top_n=10):
     user_prompt_vector = vectorizer.transform([prompt])
     sim = cosine_similarity(user_prompt_vector, matrix).flatten() 
     top_indices = sim.argsort()[-top_n:][::-1]  
-    recommended_articles = df.iloc[top_indices][['url', 'title', 'content']]
+    recommended_articles = df.iloc[top_indices][['url', 'title', 'content', 'image']]
     recommended_articles['content'] = recommended_articles['content'].apply(lambda x: x[:100] if isinstance(x, str) else x)
     return recommended_articles.T.to_dict()
